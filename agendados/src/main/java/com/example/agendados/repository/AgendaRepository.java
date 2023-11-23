6ef9210a970424a0bb55282b38322bae6294b75e
AlejandroEstech <alejandro.munoz@escuelaestech.es>
2023-11-23 21:17:35 +0100

    PSP

---
+++ agendados/src/main/java/com/example/agendados/repository/AgendaRepository.java
@@ -0,0 +1,12 @@
+package com.example.agendados.repository;
+
+import com.example.agendados.model.Contacto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AgendaRepository extends JpaRepository<Contacto,Long> {
+    List<Contacto> findByNombre(String nombre);
+}
---
+++ agendados/src/main/java/com/example/agendados/service/AgendaService.java
@@ -0,0 +1,17 @@
+package com.example.agendados.service;
+
+import com.example.agendados.repository.AgendaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendaService {
+
+    @Autowired
+    private AgendaRepository agendaRepository;
+
+
+
+
+
+}
