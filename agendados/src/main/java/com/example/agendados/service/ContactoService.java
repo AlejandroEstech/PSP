ea04439fe5795d545d6fbbfaecfce635fb59bc1a
AlejandroEstech <alejandro.munoz@escuelaestech.es>
2023-11-23 21:23:18 +0100

    PSP

--- agendados/src/main/java/com/example/agendados/service/ContactoService.java
+++ agendados/src/main/java/com/example/agendados/service/ContactoService.java
@@ -1,17 +1,14 @@
 package com.example.agendados.service;
 
-import com.example.agendados.repository.AgendaRepository;
+import com.example.agendados.repository.ContactoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AgendaService {
+public class ContactoService {
 
     @Autowired
-    private AgendaRepository agendaRepository;
-
-
-
+    private ContactoRepository agendaRepository;
 
 
 }
